2509474df7cb1df6d2c38643ce0306ab2caa4c55
kweharmony <106910677+kweharmony@users.noreply.github.com>
2024-12-15 22:21:21 +0300

    Delete my_package directory

--- my_package/__init__.py
+++
@@ -1,2 +0,0 @@
-from my_package.string_operations import *
-from my_package.number_operations import * 
--- my_package/string_operations.py
+++
@@ -1,5 +0,0 @@
-def reverse(text):
-  return text[::-1]
-
-def capitalize(text):
-  return text.capitalize()
